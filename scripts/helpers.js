4b198fc692ad222f6078c0620e3ce43af194f2e3
Flávio Dutra <flavio.adore@gmail.com>
2024-01-13 19:16:36 -0300

    feat: vite

---
+++ scripts/helpers.js
@@ -0,0 +1,10 @@
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export {
+  getRandomInt,
+}
